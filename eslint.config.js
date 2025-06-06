b277be829fc9a8b940aee6afee72bf910f5e2183
bogdanafilimonenko <bfilimonenko@gmail.com>
2025-06-06 16:10:31 +0300

    feat: update eslint, prettier, gitignore

--- eslint.config.js
+++ eslint.config.js
@@ -1,23 +1,32 @@
 import js from '@eslint/js';
 import globals from 'globals';
+import reactHooks from 'eslint-plugin-react-hooks';
+import reactRefresh from 'eslint-plugin-react-refresh';
 import tseslint from 'typescript-eslint';
-import pluginReact from 'eslint-plugin-react';
-import { defineConfig } from 'eslint/config';
-import eslintConfigPrettier from 'eslint-config-prettier/flat';
+import prettierConfig from 'eslint-config-prettier/prettier';
 
-defineConfig([
-  { ignores: ['dist'] },
+export default tseslint.config(
+  { ignores: ['node_modules/', 'dist/', 'build/'] },
   {
-    files: ['**/*.{js,mjs,cjs,ts,mts,cts,jsx,tsx}'],
-    plugins: { js },
-    extends: ['js/recommended', 'prettier'],
+    extends: [js.configs.recommended, ...tseslint.configs.recommended],
+    files: ['**/*.{ts,tsx}'],
+    languageOptions: {
+      ecmaVersion: 2020,
+      globals: globals.browser,
+    },
+    plugins: {
+      'react-hooks': reactHooks,
+      'react-refresh': reactRefresh,
+    },
+    rules: {
+      ...reactHooks.configs.recommended.rules,
+      'react-refresh/only-export-components': ['warn', { allowConstantExport: true }],
+    },
   },
   {
-    files: ['**/*.{js,mjs,cjs,ts,mts,cts,jsx,tsx}'],
-    languageOptions: { globals: { ...globals.browser, ...globals.node } },
+    name: 'with-prettier',
+    rules: {
+      ...prettierConfig.rules,
+    },
   },
-  tseslint.configs.recommended,
-  pluginReact.configs.flat.recommended,
-]);
-
-export default [defineConfig, eslintConfigPrettier];
+);
