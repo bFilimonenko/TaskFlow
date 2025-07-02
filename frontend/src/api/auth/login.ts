ff4d5c396ae1c5354e658873992566261ba44f80
bohdana-diexp-tech <bohdana.filimonenko@diexp.tech>
2025-07-02 10:38:52 +0300

    feat: use enum for endpoints

--- frontend/src/api/auth/login.ts
+++ frontend/src/api/auth/login.ts
@@ -1,7 +1,8 @@
 import { instance } from '@/api';
+import { API_ENDPOINTS } from '@/api/api-endpoints.enum.ts';
 
 export const loginRequest = async (loginValues: { email: string; password: string }) => {
-  const response = await instance.post('auth/login', loginValues);
+  const response = await instance.post(API_ENDPOINTS.AUTH_LOGIN, loginValues);
   localStorage.setItem('accessToken', response.data.accessToken);
   localStorage.setItem('refreshToken', response.data.refreshToken);
   return response.data;
--- frontend/src/api/auth/signup.ts
+++ frontend/src/api/auth/signup.ts
@@ -1,8 +1,9 @@
 import { instance } from '@/api';
+import { API_ENDPOINTS } from '@/api/api-endpoints.enum.ts';
 import type { ISignupForm } from '@/components/Signup/Signup.tsx';
 
 export const signupRequest = async (signupValues: ISignupForm) => {
-  const response = await instance.post('auth/signup', signupValues);
+  const response = await instance.post(API_ENDPOINTS.AUTH_SIGNUP, signupValues);
   localStorage.setItem('accessToken', response.data.accessToken);
   localStorage.setItem('refreshToken', response.data.refreshToken);
   return response.data;
